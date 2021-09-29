143c71317be406a8c829fd55c709d53bbdcedf8f
haleywitherell <hwitherell22@gmail.com>
2021-09-28 22:11:30 -0400

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// ReadMe Questions
+// ReadMe Object
 const readmeGenerator =(obj) => {
 const licenses = {
     "Apache License 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
@@ -60,6 +60,7 @@ ${obj.questions}`
     fs.writeFileSync("README.md", structure)
 }
 
+// ReadMe Questions
 const promptUser = () => {
     inquirer.prompt([
       {
@@ -110,17 +111,3 @@ const promptUser = () => {
   };
 
 promptUser()
-// console.log(resultObj)
-
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
